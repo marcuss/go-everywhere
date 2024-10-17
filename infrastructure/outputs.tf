a495c77489ad0a9699f3f504277eadc750f6dccc
Marcus S. <5760337+marcuss@users.noreply.github.com>
2024-10-16 22:53:56 -0500

    Full Cycle working with AWS Deployment

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,4 +1,24 @@
 # Outputs for convenience
+output "region" {
+  description = "The AWS region where resources were deployed"
+  value       = var.region
+}
+
+output "github_user" {
+  description = "The GitHub user name"
+  value       = var.github_user
+}
+
+output "github_repo" {
+  description = "The GitHub repository name"
+  value       = var.github_repo
+}
+
+output "cloud_provider" {
+  description = "The cloud provider being used"
+  value       = var.cloud_provider
+}
+
 output "eks_federated_deployer_role_name" {
   description = "The name of the IAM role for EKS federated deployer"
   value       = aws_iam_role.eks_federated_deployer.name # Output the role name
@@ -12,16 +32,6 @@ output "eks_node_role_role_name" {
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
   value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
-}
-
-output "region" {
-  description = "AWS region"
-  value       = var.region
 }
 
 output "cluster_name" {
